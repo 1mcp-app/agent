95d7bf4798cf0aafa1ea85e8e7a7859d96b4b840
Xu Zhipei <xuzhipei@gmail.com>
2026-09-04 01:26:07 +0800

    chore(gateway): remove stale test import

--- test/e2e/gateway-era-skeleton.test.ts
+++ test/e2e/gateway-era-skeleton.test.ts
@@ -12,7 +12,6 @@ import {
   LegacyOutboundEraAdapter,
   ModernInboundEraAdapter,
   ModernOutboundEraAdapter,
-  type OutboundEraAdapter,
   type ProtocolEra,
   type ProtocolEraPin,
   toImmutableJsonValue,
